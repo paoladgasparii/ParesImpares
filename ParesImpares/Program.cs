a721a33b1c12707a1f150d8f461f75a7d0087138
Usuario <Usuario@CPD03-M03>
2024-03-26 11:02:11 -0300

    Adicionar arquivos de projeto.

---
+++ ParesImpares/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParesImpares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 0 para pares ou 1 para ímpares: ");
+            int opcao = int.Parse(Console.ReadLine());
+
+            int cont = opcao;
+
+            while (cont <= 20)
+            {
+                Console.WriteLine(cont);
+                cont += 2;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
